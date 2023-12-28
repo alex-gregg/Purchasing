3918df15bcc111b19da0903fb95bb9ca544243e0
Dr Alex <84071517+alex-gregg@users.noreply.github.com>
2023-12-28 17:46:16 +1100

    Create label-issue.py

---
+++ .github/scripts/label-issue.py
@@ -0,0 +1,12 @@
+import re
+import os
+
+issue_description = os.getenv('GITHUB_EVENT_ISSUE_BODY', '')
+team_name_match = re.search(r'## Team\n([^\n]+)', issue_description)
+
+if team_name_match and team_name_match.group(1):
+    team_name = team_name_match.group(1).strip()
+    print(f'Team Name: {team_name}')
+    print(f'::set-output name=team-name::{team_name}')
+else:
+    print('::error::Team name not found in the issue description.')
