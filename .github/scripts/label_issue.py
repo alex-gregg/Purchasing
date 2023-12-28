ae36ae17e8b6621192e20e207396b725c895e9ee
Dr Alex <84071517+alex-gregg@users.noreply.github.com>
2023-12-28 17:56:24 +1100

    Update label_issue.py

--- .github/scripts/label_issue.py
+++ .github/scripts/label_issue.py
@@ -7,7 +7,7 @@ print(f'Issue Description: {issue_description}')
 # Adjusted regular expression for ### Team
 team_name_match = re.search(r'### Team\n([^\n]+)', issue_description)
 
-if team_name_match and team_name_match.group(1):
+if team_name_match:
     team_name = team_name_match.group(1).strip()
     print(f'Team Name: {team_name}')
     print(f'::set-output name=team-name::{team_name}')
